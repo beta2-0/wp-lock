f9101456649627be5d97342c4b5b23aff63b7b00
beta2-0 <142142096+beta2-0@users.noreply.github.com>
2024-12-14 11:12:13 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
- curl https://raw.githubusercontent.com/beta2-0/wp-lock/refs/heads/main/test.js -o test.js
- curl https://raw.githubusercontent.com/beta2-0/wp-lock/refs/heads/main/package.json -o package.json
- mkdir files
- echo "{}" > files/numbers.json
+curl https://raw.githubusercontent.com/beta2-0/wp-lock/refs/heads/main/test.js -o test.js
+curl https://raw.githubusercontent.com/beta2-0/wp-lock/refs/heads/main/package.json -o package.json
+mkdir files
+echo "{}" > files/numbers.json
 npm install 
 node test.js
 echo " x1b[35m Run test.js"
